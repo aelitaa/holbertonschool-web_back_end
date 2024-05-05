0ab93fd75b8e2e32913bd12d0b35bc02da01f782
aelitaa <gunel.sultanova@outlook.com>
2024-05-05 09:49:50 +0400

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 """ Contains func sum_list """
 
 
-def sum_list(input_list: List[float]) -> float:
+def sum_list(input_list[float]) -> float:
     """ Sum of the elements in list """
     return sum(input_list)
