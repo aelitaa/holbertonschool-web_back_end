a9236c71a664c931be02032ae339127550cb10ca
aelitaa <gunel.sultanova@outlook.com>
2024-05-05 09:20:13 +0400

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Contains function floor """
+import math
+
+
+def floor(n: float) -> int:
+    """ Takes a variable n and returns floor of it """
+    return math.floor(n)
