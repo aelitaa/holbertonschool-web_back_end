45cf577ad8785dc66a2d1845740b19169e2fd30e
aelitaa <gunel.sultanova@outlook.com>
2024-05-05 09:52:26 +0400

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Contains func sum_list """
+from typing import List
 
 
-def sum_list(input_list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sum of the elements in list """
     return sum(input_list)
